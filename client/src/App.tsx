bcdd2d3dbfd65850dd16ea22f6513fe044bc1155
Foxrim <Flavien085@gmail.com>
2025-01-10 09:40:18 +0100

    [fix] - position header footer avec outlet

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,14 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
+      <Header />
       <Outlet />
+      <Footer />
     </>
   );
 }
--- client/src/pages/Homepage.tsx
+++ client/src/pages/Homepage.tsx
@@ -1,5 +1,3 @@
-import Footer from "../components/Footer";
-import Header from "../components/Header";
 import HomeCollections from "../components/HomeCollections";
 import HomeLastCollection from "../components/HomeLastCollection";
 import HomeOurSelection from "../components/HomeOurSelection";
@@ -8,12 +6,10 @@ import HomeReviews from "../components/HomeReviews";
 export default function Homepage() {
   return (
     <>
-      <Header />
       <HomeLastCollection />
       <HomeOurSelection />
       <HomeReviews />
       <HomeCollections />
-      <Footer />
     </>
   );
 }
