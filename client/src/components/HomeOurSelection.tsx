2347402253b9bdea820386f5ff52a4a98bfc430d
Foxrim <Flavien085@gmail.com>
2025-01-11 16:08:02 +0100

    [feat] - fetch in OurCollection

--- client/src/components/HomeOurSelection.tsx
+++ client/src/components/HomeOurSelection.tsx
@@ -1,22 +1,41 @@
-import lac from "/photos/alpes/Lac des vaches1.jpg";
-import shinjuku from "/photos/modernTokyo/Shinjuku_night.jpg";
-import zugspitze4 from "/photos/munich/Zugspitze4.jpg";
+import { useEffect, useState } from "react";
 import styles from "../styles/HomeOurSelection.module.css";
 
+type Collection = {
+  id: number;
+  name: string;
+  image: string;
+};
+
 export default function HomeOurSelection() {
+  const [collections, setCollections] = useState<Collection[]>([]);
+
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_API_URL}/api/photos`)
+      .then((response) => response.json())
+      .then((data: Collection[]) => {
+        setCollections(data);
+      });
+  }, []);
+
+  const selectedCollections = [
+    { id: 32, className: styles.photo1 },
+    { id: 19, className: styles.photo2 },
+    { id: 7, className: styles.photo3 },
+  ];
+
   return (
     <section className={styles.ourSelection}>
       <h2>Notre sélection</h2>
       <div>
-        <figure className={styles.photo1}>
-          <img src={lac} alt="" />
-        </figure>
-        <figure className={styles.photo2}>
-          <img src={shinjuku} alt="" />
-        </figure>
-        <figure className={styles.photo3}>
-          <img src={zugspitze4} alt="" />
-        </figure>
+        {selectedCollections.map((item) => {
+          const collection = collections.find((col) => col.id === item.id);
+          return collection ? (
+            <figure key={collection.id} className={item.className}>
+              <img src={collection.image} alt={collection.name} />
+            </figure>
+          ) : null;
+        })}
       </div>
     </section>
   );
