6227b6779aa787a997558473e8f2a2b98ce099c3
JeremyPerezGit <jeremy.perez13@hotmail.fr>
2025-01-14 14:59:30 +0100

    [FIX] : fixed the magnifiying glass of the searchbar + started searchbar

--- client/src/pages/ShopPage.tsx
+++ client/src/pages/ShopPage.tsx
@@ -1,13 +1,36 @@
+// import { useState } from "react";
 import Filters from "../components/Filters";
-import SearchBar from "../components/SearchBar";
+// import SearchBar from "../components/SearchBar";
 import ShopPhotos from "../components/ShopPhotos";
 import styles from "../styles/ShopPage.module.css";
 
+// interface PhotosProps {
+//   id: string;
+//   name: string;
+//   image: string;
+//   description: string;
+//   format: string;
+//   stock: number;
+//   price: number;
+//   is_favorite: boolean;
+//   collection_id: number;
+// }
+
 export default function ShopPage() {
+  // const [photos, setPhotos] = useState<PhotosProps[] | null>([]);
+  // const [searchTerm, setSearchTerm] = useState("");
+
+  // const filteredPhotos = photos.filter((photo) =>
+  //   photo.name.toLowerCase().includes(searchTerm.toLowerCase()),
+  // // );
+  // const handleSearch = (term: string) => {
+  //   setSearchTerm(term);
+  // };
+
   return (
     <>
       <section className={styles.searchTools}>
-        <SearchBar />
+        {/* <SearchBar onSearch={handleSearch} /> */}
         <Filters />
       </section>
       <section className={styles.photos}>
