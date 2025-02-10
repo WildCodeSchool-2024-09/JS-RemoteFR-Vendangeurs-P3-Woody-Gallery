46db0f81f4cda208c88a4822c989ddb4f741b90c
Foxrim <Flavien085@gmail.com>
2025-02-10 10:34:55 +0100

    [fix] - Modal User delete css fix

--- client/src/components/AdminPage/Users/ModalDeleteUsers.tsx
+++ client/src/components/AdminPage/Users/ModalDeleteUsers.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useUsers } from "../../../contexts/AdminUsersContext";
-import styles from "../../../styles/AdminPage/Users/UserModal.module.css";
+import styles from "../../../styles/AdminPage/Articles/ModalDeleteArticle.module.css";
 
 type ModalProps = {
   handleDeleteFalse: () => void;
